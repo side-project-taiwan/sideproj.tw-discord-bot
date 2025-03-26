6b9c44da46fc3cc44e7284059a3ef003e8e13b92
Parsons <gongyingparsons@gmail.com>
2025-03-26 23:49:46 +0800

    feat: 新增SP排名指令，支持按日、週、月篩選排名

--- src/commands/economy/sprank.js
+++ src/commands/economy/sprank.js
@@ -3,6 +3,9 @@ const {
   Interaction,
 } = require("discord.js");
 const Level = require("../../models/Level");
+const SpExpChange = require("../../models/SpExpChange");
+const { DateTime, Settings } = require("luxon");
+Settings.defaultZone = "Asia/Taipei";
 module.exports = {
   /**
    *
@@ -14,12 +17,57 @@ module.exports = {
       interaction.reply("This command is only available inside servers.");
       return;
     }
-
-    const top10users = await Level.find({
-      guildId: interaction.guild.id,
-    }).sort({ spExp: -1 }).limit(10);
-    // console.log('top10users: ',top10users);
-    
+    const duration = interaction.options.getString('duration') || 'all';
+    let top10users = [];
+    if(duration === 'all'){
+      top10users = await Level.find({
+        guildId: interaction.guild.id,
+      }).sort({ spExp: -1 }).limit(10);
+    } else {
+      const now = DateTime.now();
+      let startTime = now.startOf('day').toJSDate();
+      switch (duration) {
+        case 'daily':
+          break;
+        case 'weekly':
+          startTime = now.startOf('week').toJSDate();
+          break;
+        case 'monthly':
+          startTime = now.startOf('month').toJSDate();
+          break;
+        default:
+          break;
+      }
+      top10users = await SpExpChange.aggregate([
+        {
+          $match: {
+            guildId: interaction.guild.id,
+            timestamp: {
+              $gte: startTime,
+            },
+          },
+        },
+        {
+          $group: {
+            _id: "$userId",
+            spExp: { $sum: "$expChange" },
+          },
+        },
+        {
+          $sort: { spExp: -1 },
+        },
+        {
+          $limit: 10,
+        },
+        {
+          $project: {
+            userId: "$_id",
+            spExp: 1,
+            _id: 0,
+          },
+        }
+      ]);
+    }
     const userIds = top10users.map((user) => user.userId);
     const users = await interaction.guild.members.fetch({user: userIds});
     const getRankingIcon = (index, userId) => {
@@ -41,10 +89,36 @@ module.exports = {
         const userInfo = users.find((user) => user.id === top10user.userId);
         return `${index + 1}. ${getRankingIcon(index, top10user.userId)} ${userInfo.displayName}(${top10user.spExp})`;
     });
-    await interaction.reply(`Ranking of SP exp:\n ${rankingLabels.join("\n")}`);
+    await interaction.reply(`Ranking of SP exp(${duration}):\n ${rankingLabels.join("\n")}`);
   },
 
   //base command data
   name: "sp-ranking",
   description: "Shows voice channel members list",
+  options: [
+    {
+      name: 'duration',
+      description: 'The duration of the ranking',
+      type: 3,
+      required: false,
+      choices: [
+        {
+          name: '今日',
+          value: 'daily',
+        },
+        {
+          name: '本週',
+          value: 'weekly',
+        },
+        {
+          name: '本月',
+          value: 'monthly',
+        },
+        {
+          name: '全部時間',
+          value: 'all',
+        },
+      ],
+    }
+  ]
 };
