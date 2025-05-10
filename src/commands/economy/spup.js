264791851ef97156b8523fe30dd2437140958a8d
Parsons <gongyingparsons@gmail.com>
2025-05-10 08:52:52 +0800

    refactor: 依照建議修改

--- src/commands/economy/spup.js
+++ src/commands/economy/spup.js
@@ -7,10 +7,9 @@ const {
   Client,
   Interaction,
 } = require("discord.js");
-
+const { getOrCreateUser } = require("../../services/level.service");
+const { getOrCreateInventory } = require("../../services/inventory.service");
 const MileageShopItem = require("../../models/MileageShopItem");
-const Level = require("../../models/Level");
-const Inventory = require("../../models/Inventory");
 
 module.exports = {
   name: "spup",
@@ -33,28 +32,19 @@ module.exports = {
     if (guildId !== env.DISCORD_GUILD_ID) return;
 
     const allItems = await MileageShopItem.find();
+    
     // 取得使用者資料
-    let userLevel = await Level.findOne({
-      userId,
-      guildId,
-    });
-    if (!userLevel) {
-      // 沒有的話初始使用者
-      userLevel = new Level({
-        userId,
-        guildId,
-        xp: 0,
-        activity: 0,
-        mileage: 0,
-        level: 0,
-        spExp: 0,
-        spSigninCooldown: Date.now() + 60 * 60 * 1000,
-      });
+    const [userLevel, inventory] = await Promise.all([
+      getOrCreateUser(userId, guildId),
+      getOrCreateInventory(userId, guildId),
+    ]);
+    // 是否有升級道具
+    let upItems = [];
+    if(inventory.items.length > 0){
+      upItems = inventory.items.filter((item)=>{
+        return ["job_scroll", "wisdom_crystal", "qigu_egg"].includes(item.key)
+      })
     }
-    const inventory = await Inventory.findOne({
-      userId,
-      guildId,
-    });
     const embed = new EmbedBuilder()
       .setTitle("SP等級提升")
       .setDescription("【 **請選擇要使用的道具** 】")
@@ -71,12 +61,8 @@ module.exports = {
           inline: false,
         }
       );
-    let upItems = [];
-    if(inventory && inventory.items.length > 0){
-      upItems = inventory.items.filter((item)=>{
-        return ["job_scroll", "wisdom_crystal", "qigu_egg"].includes(item.key)
-      })
-    }
+    
+
     if(upItems.length === 0){
       embed.setColor(0xff0000)
       embed.addFields({name:"提醒",value: "您目前沒有可用於升等的道具，請先取得道具再來升級！", inline: false});
@@ -86,6 +72,10 @@ module.exports = {
 
     for (let i = 0; i < upItems.length; i++) {
       const item = allItems.find((item) => item.key === upItems[i].key);
+      if (!item){
+        console.warn(`Item with key ${upItems[i].key} not found in allItems.`);
+        continue;
+      }
       const button = new ButtonBuilder()
         .setCustomId(`spup_${item.key}`)
         .setLabel(`${item.name}(持有${upItems[i].quantity})`)
