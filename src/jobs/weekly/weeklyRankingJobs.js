a18ef6f8b8340693349b10c84c18b02c4f27bbaa
Ian Chien <err@csie.io>
2025-07-07 23:08:11 +0800

    Add weekly ranking cron job

--- src/jobs/weekly/weeklyRankingJobs.js
+++ src/jobs/weekly/weeklyRankingJobs.js
@@ -1,10 +1,19 @@
-const { getClient } = require('../../services/time.service');
-
-
 module.exports = {
   name: 'update-weekly-ranking',
-  cron: '0 0 * * 1',
+  // Run at 23:59 Taiwan time (UTC+8) on Sunday
+  cron: '59 15 * * 0', // 週日 UTC+0 的 15:59 = 台灣時間週日 23:59
   handler: async () => {
-    // TODO: Add Weekly Jobs
+    const now = new Date().toLocaleDateString("zh-TW", { timeZone: "Asia/Taipei" });
+    const client = getClient();
+    const guild = await client.guilds.fetch(env.DISCORD_GUILD_ID);
+    const channel = await client.channels.fetch(env.channels.sp);
+    const label = `${now} Weekly`;
+
+    const { teamInfo } = await fetchRankingData(guild, "weekly");
+    const imageBuffer = await drawSpRanking(teamInfo, label);
+
+    await channel.send({
+      files: [{ attachment: imageBuffer, name: "weekly-ranking.png" }],
+    });
   },
 };
